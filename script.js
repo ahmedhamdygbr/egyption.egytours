1f2d57a1e07535ca918ef9f89bf40857a11ebc98
ahmed hamdy <am1325814@gmail.com>
2026-05-04 12:15:44 +0300

    ssssss

--- script.js
+++ script.js
@@ -1,37 +1,35 @@
+function sc(btn , direction) {
+    const parent = btn.parentElement;
+    const ascroll = parent.querySelector('.bscroll');
+    const scroll = 220;
+    ascroll.scrollBy({
+        left: direction * scroll,
+        behavior: "smooth"
+    })
+}
 
-// 1. تحديد العناصر من الـ DOM
-const loginForm = document.querySelector('.login-form');
-const usernameInput = document.querySelector('input[type="text"]');
-const passwordInput = document.querySelector('input[type="password"]');
-
-// 2. إضافة حدث عند الضغط على زرار السابمت
-loginForm.addEventListener('submit', function(e) {
-    // منع الصفحة من التحميل (Refresh)
-    e.preventDefault();
-
-    const username = usernameInput.value.trim();
-    const password = passwordInput.value.trim();
-
-    // --- أولاً: الـ Local Validation (التأكد من البيانات محلياً) ---
-    if (username === "" || password === "") {
-        alert("يا ريس لازم تكتب اليوزر نيم والباسورد الأول!");
-        return; // وقف الكود هنا ومكملش
+const tdark = document.getElementById("tdark");
+let dark = localStorage.getItem("dark");
+const yesdark = () => {
+    document.body.classList.add("dark");
+    localStorage.setItem("dark", "active");
+}
+const nodark = () => {
+    document.body.classList.remove("dark");
+    localStorage.setItem("dark", null);
+}
+if (dark !== "active") {
+    nodark();
+} 
+else {
+    yesdark();
+}
+tdark.addEventListener("click", () => {
+    dark = localStorage.getItem("dark");
+    if (dark !== "active") {
+        yesdark();
     }
-
-    if (password.length < 6) {
-        alert("الباسورد ضعيف، لازم يكون 6 حروف على الأقل.");
-        return;
+    else {
+        nodark();
     }
-
-    // --- ثانياً: الـ Local Storage (تخزين البيانات في المتصفح) ---
-    // بنخزن اسم المستخدم عشان نرحب بيه بعدين أو نفتكره
-    localStorage.setItem('user_name', username);
-    
-    alert("تم تسجيل الدخول بنجاح يا " + username);
-
-    // تحويل المستخدم لصفحة تانية مثلاً
-    // window.location.href = "dashboard.html";
-});
-
-
-
+})
